6172a8e40dc806a3157eac7005bfcdc2a687d8ec
Ink-33 <quhyu@qq.com>
2020-06-27 15:19:14 +0800

    SmlkShell

--- smlkshell/command.go
+++ smlkshell/command.go
@@ -3,7 +3,15 @@ package smlkshell
 import (
 	"SMLKBOT/botstruct"
 	"SMLKBOT/cqfunction"
+	"fmt"
+	"log"
+	"runtime"
+	"strings"
+	"time"
 )
+
+//CompileDate
+var cd string
 
 //Reload config.
 func Reload(MsgInfo *botstruct.MsgInfo, MasterID string) (result bool) {
@@ -17,32 +25,78 @@ func Reload(MsgInfo *botstruct.MsgInfo, MasterID string) (result bool) {
 
 //SmlkShell is the shell of SMLKBOT
 func SmlkShell(MsgInfo *botstruct.MsgInfo, BotConfig *botstruct.BotConfig) {
-
+	if strings.HasPrefix(MsgInfo.Message, "<SMLK ") {
+		if RoleHandler(MsgInfo, BotConfig).RoleLevel >= 1 {
+			switch MsgInfo.Message {
+			case "<SMLK status":
+				m := new(runtime.MemStats)
+				runtime.ReadMemStats(m)
+				msgMake := fmt.Sprintf("$SMLKBOT>\nBuild with: %s\nBuild Arch&OS: %s-%s\nBuild Date: %s\nNumGoroutine: %d\nNumCPU: %d\nMemory: %dBytes\nNumGC: %d\nForceGC: %d\nLsatGC:%s", runtime.Version(), runtime.GOARCH, runtime.GOOS, cd, runtime.NumGoroutine(), runtime.NumCPU(), m.Sys, m.NumForcedGC, m.NumGC, time.Unix(0, int64(m.LastGC)).Format("2006-01-02 15:04:05"))
+				log.Println(msgMake)
+				ShellLog(MsgInfo, BotConfig, msgMake)
+				break
+			case "<SMLK gc":
+				if RoleHandler(MsgInfo, BotConfig).RoleLevel == 3 {
+					runtime.GC()
+					ShellLog(MsgInfo, BotConfig, "succeed")
+				} else {
+					ShellLog(MsgInfo, BotConfig, "deny")
+				}
+				break
+			default:
+				ShellLog(MsgInfo, BotConfig, "notfound")
+			}
+		} else {
+			ShellLog(MsgInfo, BotConfig, "deny")
+		}
+	}
 }
 
 //RoleHandler : Fechting user's role.
-func RoleHandler(MsgInfo *botstruct.MsgInfo, BotConfig *botstruct.BotConfig) (role string) {
-	role = ""
+func RoleHandler(MsgInfo *botstruct.MsgInfo, BotConfig *botstruct.BotConfig) (role *botstruct.Role) {
+	role = new(botstruct.Role)
+	role.RoleName = "member"
+	role.RoleLevel = 0
 	for _, v := range BotConfig.MasterID {
 		if MsgInfo.SenderID == v.String() {
-			role = "master"
+			role.RoleLevel = 3
+			role.RoleName = "master"
 			return
 		}
 	}
 	if MsgInfo.MsgType == "group" {
-		role = MsgInfo.Role
-		return
+		role.RoleName = MsgInfo.Role
+		switch role.RoleName {
+		case "owner":
+			role.RoleLevel = 2
+			return
+		case "admin":
+			role.RoleLevel = 1
+			return
+		default:
+			return
+		}
 	}
 	return
 }
 
 //ShellLog : Send execute result to QQ
-func ShellLog(MsgInfo *botstruct.MsgInfo, BotConfig *botstruct.BotConfig, result bool) {
-	var msgmake string
-	if result {
-		msgmake = "$SmlkShell> Succeed."
-	} else {
-		msgmake = "$SmlkShell> Permission denied."
+func ShellLog(MsgInfo *botstruct.MsgInfo, BotConfig *botstruct.BotConfig, result string) {
+	var msgMake string
+	switch result {
+	case "succeed":
+		msgMake = "$SmlkShell> Succeed."
+		break
+	case "deny":
+		msgMake = "$SmlkShell> Permission denied."
+		break
+	case "notfound":
+		msgMake = fmt.Sprintf("$SmlkShell> %s: command not found", strings.Replace(MsgInfo.Message, "<SMLK ", "", 1))
+		break
+	default:
+		msgMake = result
 	}
-	cqfunction.CQSendMsg(MsgInfo, msgmake, BotConfig)
+	if msgMake != "" {
+		cqfunction.CQSendMsg(MsgInfo, msgMake, BotConfig)
+	}
 }
