6d3b9c4f87edfef35d39030490cbc57cc9cf5129
Ink-33 <quhyu@qq.com>
2020-07-29 01:59:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ echo "Welcome to use SMLKBOT!"
 if [ "$1" == "TencentSCF" ]; then
     echo "Build SMLKBOT for Tencent SCF..."
     flags="-X 'SMLKBOT/utils/smlkshell.date=$(date)' -X 'SMLKBOT/utils/smlkshell.version="DevBuild-SCF"' -X 'SMLKBOT/utils/smlkshell.commit="$(git rev-parse --short HEAD)"' -X 'SMLKBOT/utils/smlkshell.IsSCF="${1}"'"
+    go build -ldflags "$flags" -o SMLKBOTSCF
     echo "Build succeed!"
 elif [ "$1" == "HTTP" ]; then
     echo "Build SMLKBOT for common"
