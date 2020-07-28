1566194b2254ac52f00aa4ca27466142c383a547
Ink-33 <quhyu@qq.com>
2020-07-29 01:47:18 +0800

    Update build script and gitignore

---
+++ build.sh
@@ -0,0 +1,17 @@
+echo "Welcome to use SMLKBOT!"
+if [ "$1" == "TencentSCF" ]; then
+    echo "Build SMLKBOT for Tencent SCF..."
+    flags="-X 'SMLKBOT/utils/smlkshell.date=$(date)' -X 'SMLKBOT/utils/smlkshell.version="DevBuild-SCF"' -X 'SMLKBOT/utils/smlkshell.commit="$(git rev-parse --short HEAD)"' -X 'SMLKBOT/utils/smlkshell.IsSCF="${1}"'"
+    echo "Build succeed!"
+elif [ "$1" == "HTTP" ]; then
+    echo "Build SMLKBOT for common"
+    flags="-X 'SMLKBOT/utils/smlkshell.date=$(date)' -X 'SMLKBOT/utils/smlkshell.commit="$(git rev-parse --short HEAD)"'"
+    go build -ldflags "$flags"
+    echo "Build succeed!"
+else
+    echo "Oops, it seems you don't set build goal..."
+    echo "Usage: bash build.sh [goal]"
+    echo "Support goals:
+    TencentSCF
+    HTTP"
+fi
