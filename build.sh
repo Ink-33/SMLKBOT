a692659d3561b862b488cd6120bc980617515d50
Ink-33 <quhyu@qq.com>
2020-07-29 22:33:29 +0800

    [ci skip] Update build script

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 echo "Welcome to use SMLKBOT!"
 if [ "$1" == "TencentSCF" ]; then
     echo "Build SMLKBOT for Tencent SCF..."
-    flags="-X 'SMLKBOT/utils/smlkshell.date=$(date)' -X 'SMLKBOT/utils/smlkshell.version="DevBuild-SCF"' -X 'SMLKBOT/utils/smlkshell.commit="$(git rev-parse --short HEAD)"' -X 'SMLKBOT/utils/smlkshell.IsSCF="${1}"'"
+    flags="-s -w -X 'SMLKBOT/utils/smlkshell.date=$(date)' -X 'SMLKBOT/utils/smlkshell.version="DevBuild-SCF"' -X 'SMLKBOT/utils/smlkshell.commit="$(git rev-parse --short HEAD)"' -X 'SMLKBOT/utils/smlkshell.IsSCF="${1}"'"
     go build -ldflags "$flags" -o SMLKBOTSCF
     echo "Build succeed!"
 elif [ "$1" == "HTTP" ]; then
     echo "Build SMLKBOT for common"
-    flags="-X 'SMLKBOT/utils/smlkshell.date=$(date)' -X 'SMLKBOT/utils/smlkshell.commit="$(git rev-parse --short HEAD)"'"
+    flags="-s -w -X 'SMLKBOT/utils/smlkshell.date=$(date)' -X 'SMLKBOT/utils/smlkshell.commit="$(git rev-parse --short HEAD)"'"
     go build -ldflags "$flags"
     echo "Build succeed!"
 else
